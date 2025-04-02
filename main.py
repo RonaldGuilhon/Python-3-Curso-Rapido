8abfd7e50df23de6aa84fa2bb32cb81f936c4a53
Ronald Simplicio <ronaldguilhon@gmail.com>
2025-04-01 22:52:19 -0400

    atualização

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!python
+#print('Ola mundo!')
+#import pacote.sub.arquivo
 
-print('Ola mundo!')
-
-import pacote.sub.arquivo
+#import tipos.variaveis
+from tipos import variaveis
---
+++ tipos/variaveis.py
@@ -0,0 +1,8 @@
+a = 3
+b = 4.4
+print(a+b)
+
+texto = 'Sua idade e ...'
+idade = 23
+print( texto + str(idade))
+print(f'{texto}{idade}')
