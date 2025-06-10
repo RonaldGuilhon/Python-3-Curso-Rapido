fc1f4d0977420dc0c2d60aa34fff25ae7901cfa9
Ronald Simplicio <ronaldguilhon@gmail.com>
2025-06-10 17:03:45 -0400

    atualização

--- tipos/tuplas.py
+++ tipos/tuplas.py
@@ -1,6 +1,8 @@
 nomes = ('Ana', 'Bia', 'Gui', 'Ana')
 
 print('bia' in nomes)
-
+print(nomes[0])
+print(nomes[1:3]) #exibe os elementos da lista de 1 a
+print(nomes[:1] ) #exibe os elementos da lista de 0 a 1
 print(type(nomes))
 print(nomes)
