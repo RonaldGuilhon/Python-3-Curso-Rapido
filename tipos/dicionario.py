14e97101d03acb0ff696de6739d085dde1fcf690
Ronald Simplicio <ronaldguilhon@gmail.com>
2025-06-10 17:29:51 -0400

    atualização

---
+++ tipos/dicionario.py
@@ -0,0 +1,13 @@
+aluno = {
+    'nome': 'João',
+    'idade': 20,
+    'curso': 'Ciência da Computação',
+    'nota': 9.2,
+    'ativo': True
+}
+
+print(type(aluno))
+print(aluno['nome'])
+print(aluno['nota'])
+print(aluno['ativo'])
+print(len(aluno))
