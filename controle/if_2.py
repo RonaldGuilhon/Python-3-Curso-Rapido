866fcf41e30cee7044fe1c9680e1eca68092a228
Ronald Simplicio <ronaldguilhon@gmail.com>
2025-06-11 18:42:34 -0400

    atualização

--- controle/if_2.py
+++ controle/if_2.py
@@ -1,12 +1,12 @@
-a = 'valor'
-a = 0
-a = -0.00001
-a = ''
-a = ' '
-a = []
-a = {}
+a = 'valor'       # True - string com conteúdo
+a = 0             # False - número zero
+a = -0.00001      # True - número diferente de zero
+a = ''            # False - string vazia
+a = ' '           # True - espaço em branco ainda é um caractere
+a = []            # False - lista vazia
+a = {}            # False - dicionário vazio
 
 if a:
-    print('Existe!')
+    print('Existe!')  # Só executa se a for "truthy"
 else:
-    print('Não existe ou zero ou vazio...')
+    print('Não existe ou zero ou vazio...')  # Executa se a for "falsy"
