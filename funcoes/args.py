e1b676fd96c4fd04d99429bce7a4881a12ba935d
Ronald Simplicio <ronaldguilhon@gmail.com>
2025-06-15 01:11:35 -0400

    atualização

--- funcoes/args.py
+++ funcoes/args.py
@@ -4,3 +4,9 @@ def soma(*nums):
     for n in nums:
         total += n
     return total
+
+def resultado_final(**kwargs):
+    status = 'aprovado' if kwargs['nota'] >= 7 else 'reprovado'
+    return f'{kwargs["nome"]} está {status} com nota {kwargs["nota"]}.'
+   
+   
--- main.py
+++ main.py
@@ -41,3 +41,6 @@ from funcoes import args
 
 s = args.soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 print(s)
+
+resultado = args.resultado_final(nome='João', nota=7.3)
+print(resultado)
